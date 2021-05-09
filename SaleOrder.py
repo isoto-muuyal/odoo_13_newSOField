636bf7e172f6c38959af24a22d4ee2e0e27a8dee
Israel Soto Salcido <israel.soto@muuyal.mx>
2021-05-09 17:38:57 -0600

    logger added

--- SaleOrder.py
+++ SaleOrder.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 from odoo import api, models, fields
 from odoo.exceptions import AccessError, UserError, ValidationError
-
+import logging
+_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     additional_note = fields.Char(string='Additional Note')
-
     barcode_shipping = fields.Char(string='Numero de rastreo')
 
     @api.constrains('barcode_shipping')
     def _check_barcode_unique(self):
         barcode_counts = self.search_count([('barcode_shipping', '=', self.barcode_shipping), ('id', '!=', self.id)])
-        print("executing barcode shipping validation!!!!!!!!")
-        print(barcode_counts)
+        _logger.info("executing barcode shipping validation!!!!!!!!")
+        _logger.info(barcode_counts)
+
         if barcode_counts > 0:
             raise ValidationError("Codigo  de rastreo ya en uso")
