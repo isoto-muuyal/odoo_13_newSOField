cdd980e8d9b93bf5ee02431899655fd67dbaffee
Israel Soto Salcido <israel.soto@muuyal.mx>
2021-06-07 21:13:27 -0600

    new app

---
+++ odoosyncapp/src/main/java/com/muuyal/iss/App.java
@@ -0,0 +1,14 @@
+package com.muuyal.iss;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
